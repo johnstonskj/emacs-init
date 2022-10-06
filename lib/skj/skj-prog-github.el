0986b7fe9337498c94a78a214a6cc27d8740f3c5
Simon Johnston <johnstonskj@gmail.com>
2022-10-06 11:05:48 -0700

    Disable Github notifier for now

--- lib/skj/skj-prog-github.el
+++ lib/skj/skj-prog-github.el
@@ -2,10 +2,10 @@
 
 (init-message "Setting up GitHub integrations" 'skj-prog-github)
 
-(require 'skj-secrets)
-
-(setq github-notifier-token (skj-secrets-value 'github-token))
-
-(add-hook 'prog-mode-hook #'github-notifier-mode)
+;; (require 'skj-secrets)
+;; 
+;; (setq github-notifier-token (skj-secrets-value 'github-token))
+;; 
+;; (add-hook 'prog-mode-hook #'github-notifier-mode)
 
 (provide 'skj-prog-github)
