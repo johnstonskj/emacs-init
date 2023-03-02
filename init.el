f930519761481e38f6da2cfc2ea34076e24e7108
Simon Johnston <johnstonskj@gmail.com>
2023-03-02 10:38:46 -0800

    Refactor and move to org-init

--- init.el
+++ init.el
@@ -1,192 +1,25 @@
-;;; init.el --- Emacs configuration -*- lexical-binding: t; -*-
+;;; init.el --- Emacs configuration Bootstrap -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;;;
+;;; Bootstrap the rest of the initialization as org files.
 
 ;;; Code:
 
-;; --------------------------------------------------------------------------
-;; Initialize customization
-
-(defgroup skj nil
-  "Simon's personal environment settings."
-  :tag "skj"
-  :prefix "skj-")
-
-(defcustom
-  skj-trace-init
-  (if (and (boundp debug-on-error) debug-on-error)
-      debug-on-error
-    nil)
-  "Emit tracing messages during initialization, useful with --debug-init."
-  :tag "Trace initialization process"
-  :group 'skj
-  :type 'boolean)
-
-(defcustom
-  skj-primary-email
-  "johnstonskj@gmail.com"
-  "Primary, non-work, email address."
-  :tag "Primary email address"
-  :group 'skj
-  :type 'string)
-
-(defcustom
-  skj-full-name
-  "Simon Johnston"
-  "Name to go with email-address."
-  :tag "Primary email address"
-  :group 'skj
-  :type 'string)
-
-(defun init-message (message &optional module)
-  "Write MESSAGE to the standard message buffer. The optional MODULE argument
-is a symbol that denotes the calling module. This is only enabled if the value
-of `skj-trace-init` is non-nil."
-  (unless (eq skj-trace-init nil)
-    (if module
-        (message "Init [%s] %s" module message)
-      (message "Init %s" message))))
-
 (defun concat-path (base-dir path)
-  "Concatenate PATH components, returning an expanded path.
-
+  "Concatenate the BASE-DIR and PATH components, returning an expanded path.
 Note that if BASE-DIR is not absolute, or expands to an absolute
 path, the result is relative to the current buffer path."
   (expand-file-name (concat (file-name-as-directory base-dir) path)))
 
-;; --------------------------------------------------------------------------
-(init-message "Configure execution path")
 
-;; (defun shell-path->list (&optional varname)
-;;   (split-string (delete-dups (getenv (if varname varname "PATH")) ":")))
-
-(defun exec-path-prepend (path)
-  "Add PATH to the beginning of the current `exec-path` variable,
-and the shell's `$PATH` variable."
-  (unless (member path exec-path)
-    (setq exec-path (cons path exec-path))
-    (setenv "PATH" (concat path ":" (getenv "PATH")))))
-
-(defun exec-path-append (path)
-  "Add PATH to the end of the current `exec-path` variable, and
-the shell's `$PATH` variable."
-  (unless (member path exec-path)
-    (setq exec-path (append exec-path (list path)))
-    (setenv "PATH" (concat (getenv "PATH") ":" path))))
-
-(when (eq system-type 'darwin)
-  (exec-path-prepend "/opt/homebrew/bin"))
-
-(when (display-graphic-p)
-  (exec-path-prepend "usr/local/bin"))
-
-(init-message (format "exec-path: %s" exec-path))
+(defvar skj/org-init
+  (concat-path user-emacs-directory "org-init")
+  "The location of the org-files used for initialization.")
 
 ;; --------------------------------------------------------------------------
-(init-message "Override default user details")
 
-(setq
- user-login-name "johnstonskj" ;; use GitHub ID here
- user-full-name skj-full-name
- user-mail-address skj-primary-email)
-
-(defvar user-home-directory (expand-file-name "~"))
-
-;; --------------------------------------------------------------------------
-(init-message "Setting customize values")
-
-;; Do not use `init.el` for `custom-*` code - use `custom.el`.
-(defvar emacs-customization-file
-  (concat-path user-emacs-directory "custom.el"))
-
-;; Assuming that the code in custom-file is execute before the code
-;; ahead of this line is not a safe assumption. So load this file
-;; proactively.
-(load-file emacs-customization-file)
-
-;; --------------------------------------------------------------------------
-;; Initialize Packages
-
-(init-message "Setting load path")
-(add-to-list 'load-path (concat-path user-emacs-directory "lib"))
-(add-to-list 'load-path (concat-path user-emacs-directory "lib/skj"))
-
-(require 'skj-packages)
-(require 'skj-dirs)
-
-
-;; --------------------------------------------------------------------------
-;; Generic Configuration
-
-(require 'skj-ui)
-(require 'skj-ui-completion)
-
-(when (eq system-type 'darwin)
-  (require 'skj-ui-mac))
-
-(require 'skj-location)
-
-(require 'skj-check)
-
-(require 'skj-shell)
-
-(require 'skj-remote)
-
-(require 'skj-git)
-(require 'skj-github)
-
-(require 'skj-org)
-
-(require 'skj-writing)
-
-(require 'skj-snippets) ;; depends on org
-
-;; --------------------------------------------------------------------------
-;; Developer Configuration
-
-(require 'skj-prog-ui)
-
-(require 'skj-prog-project)
-
-(require 'skj-prog-dap)
-(require 'skj-prog-lsp)
-(require 'skj-prog-test)
-
-(require 'skj-prog-data)
-(require 'skj-prog-lispy)
-(require 'skj-prog-rust)
-(require 'skj-prog-web)
-
-(require 'skj-prog-services)
-
-;; --------------------------------------------------------------------------
-;; Work Configuration
-
-(require 'skj-work)
-
-;; --------------------------------------------------------------------------
-;; One last thing, ...
-(init-message "Checking for server")
-
-(require 'server)
-(unless (and (fboundp 'server-running) (server-running-p))
-  (server-start))
+(org-babel-load-file (concat-path skj/org-init "init.org"))
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(sanityinc-solarized-light))
- '(package-selected-packages
-   '(lsp-scheme prettify-math prettify-greek fira-code-mode auto-dictionary org-fancy-priorities org-clock-reminder org-gcal org-agenda-property org-analyzer org-beautify-theme org-bullets org-habit-stats org-hyperscheduler org-sync org-tag-beautify smartparens dr-racket-like-unicode flycheck-pest flycheck-projectile geiser-gambit geiser-gauche macrostep-geiser coverage coverlay web-beautify web-mode xref-js2 js2hl js2-refactor js2-highlight-vars js2-mode zmq vterm wakatime-mode emacsql-sqlite-module toml-mode flycheck-yamllint yaml-mode json-mode csv-mode pcsv flycheck-package package-lint scheme-complete quack geiser-racket geiser-chez elisp-slime-nav slime-repl-ansi-color slime-company slime scribble-mode toml pest-mode flycheck-rust rustic rust-playground rust-auto-use cargo-mode tabspaces find-file-in-project project-persist project-explorer projectile-ripgrep projectile-git-autofetch verb dap-mode lsp-ui lsp-mode diff-hl forge magithub magit-todos magit-filenotify magit-lfs bug-reference-github github-review github-pullrequest github-notifier github-explorer github-browse-file git-modes git-timemachine git-link git-blamed git-attr smog bibtex-utils bibretrieve latex-extra latex-math-preview latex-preview-pane focus jinja2-mode easy-jekyll markdown-mode license-snippets gitignore-snippets yasnippet-snippets hl-todo flyspell-correct-ivy ivy-todo ivy-yasnippet ivy-rich ivy-prescient ivy-omni-org ivy-historian ivy-file-preview ivy-explorer ivy-emoji ivy-bibtex smex counsel-web counsel-test counsel-projectile counsel-osx-app counsel-org-capture-string counsel-fd counsel company-web company-quickhelp company-prescient company-org-block company-math company-emoji company-bibtex company mexican-holidays flycheck-plantuml plantuml-mode graphviz-dot-mode ox-wk ox-gfm ob-rust ob-latex-as-png org-timeline org-superstar org-sticky-header org-sidebar org-ref-prettify org-ql org-modern org-doing org-alert osx-trash reveal-in-osx-finder osx-plist osx-org-clock-menubar osx-lib page-break-lines smart-tabs-mode rainbow-delimiters aggressive-indent highlight-indent-guides undo-tree persist everlasting-scratch mc-extras multiple-cursors multicolumn color-theme-sanityinc-solarized ibuffer-sidebar ibuffer-projectile ibuffer-git dired-sidebar dir-treeview buffer-move buffer-expose major-mode-icons mode-icons all-the-icons-dired all-the-icons-completion all-the-icons)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#fdf6e3" :foreground "#657b83" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight light :height 140 :width normal :foundry "nil" :family "Fira Code"))))
- '(focus-unfocused ((t (:inherit font-lock-comment-face :foreground "white"))))
- '(tty-menu-disabled-face ((t (:background "brightgreen" :foreground "lightgray"))))
- '(tty-menu-enabled-face ((t (:background "brightgreen" :foreground "brightwhite" :weight bold))))
- '(tty-menu-selected-face ((t (:background "brightmagenta")))))
--- lib/skj/skj-secrets.el
+++ lib/skj/skj-secrets.el
@@ -2,29 +2,27 @@
 
 ;;; Code:
 
-(init-message "Setting up secrets manager" 'skj-secrets)
-
 (require 'epg-config)
 
 (setq
  epg-gpg-program "gpg2"
  epg-gpg-home-directory (expand-file-name "~/.gnupg"))
 
-(init-message (format "Using command: %s, as %s" epg-gpg-program (executable-find epg-gpg-program)) 'skj-secrets)
+(message (format "[skj-secrets] Using command: %s, as %s" epg-gpg-program (executable-find epg-gpg-program)))
 
 ;; --------------------------------------------------------------------------
 
 (require 'xdg)
 
 (defcustom
-  skj-secrets-file
+  skj/secrets-file
   (concat-path (xdg-config-home) "emacs/secrets.el.gpg")
   "This is a GPG-encrypted Emacs Lisp file that will be loaded to allow the
 addition of secret values securely. If set to nil no attempt to load any
 external secrets will be attempted.
 
 The contents of this file can retrieve secrets from other applications or
-simply store them in source and use the function ‘skj-secrets-add’ to add
+simply store them in source and use the function ‘skj/secrets-add’ to add
 them to the secret store. The referenced file should have the extension
 '.el.gpg' to enable auto decryption on load."
   :tag "External, secure, secrets file"
@@ -33,24 +31,24 @@ them to the secret store. The referenced file should have the extension
 
 ;; --------------------------------------------------------------------------
 (defvar
-  skj-secrets--alist
+  skj/secrets--alist
   '()
   "The internal secret store, it is implemented as an association list.
 
 The ‘car’ of each pair is expected to be a symbol, the ‘cdr’ is any
 value. The structure of this value can be validated by the function
-‘skj-secrets--validate’ and a value can be retrieved using the
-function ‘skj-secrets-value’. It is recommended to use the function
-‘skj-secrets-add’ to add a new key and secret.")
+‘skj/secrets--validate’ and a value can be retrieved using the
+function ‘skj/secrets-value’. It is recommended to use the function
+‘skj/secrets-add’ to add a new key and secret.")
 
 
 ;; --------------------------------------------------------------------------
 (require 'seq)
 
-(defun skj-secrets--validate ()
-  "Return t if the value of the variable ‘skj-secrets-alist’ is
+(defun skj/secrets--validate ()
+  "Return t if the value of the variable ‘skj/secrets-alist’ is
 correct, or signal an error."
-  (if (proper-list-p skj-secrets--alist)
+  (if (proper-list-p skj/secrets--alist)
       (seq-every-p
        (lambda (pair)
          (cond
@@ -59,36 +57,36 @@ correct, or signal an error."
           ((not (symbolp (car pair)))
            (error "Error [skj-secrets] pair's car (%s) is not a symbol" (car pair)))
           (t)))
-       skj-secrets--alist)
+       skj/secrets--alist)
     (error "Error [skj-secrets] the secrets list is not a proper list")))
 
 
 ;; --------------------------------------------------------------------------
-(defun skj-secrets-add (key secret)
+(defun skj/secrets-add (key secret)
   "Associate the SECRET value with the provided symbol KEY.
 
 This function returns t if the value is added successful, else nil."
-  (skj-secrets--validate)
+  (skj/secrets--validate)
   (cond
    ((not (symbolp key))
-    (message "Warning [skj-secrets] key (%s) is not a symbol" key)
+    (message "[skj-secrets] Warning key (%s) is not a symbol" key)
     nil)
-   ((not (eq (assoc key skj-secrets--alist) nil))
-    (message "Warning [skj-secrets] key (%s) already exists as a secret key" key)
+   ((not (eq (assoc key skj/secrets--alist) nil))
+    (message "[skj-secrets] Warning key (%s) already exists as a secret key" key)
     nil)
    (t
     (progn
-        (add-to-list 'skj-secrets--alist (cons key secret))
+        (add-to-list 'skj/secrets--alist (cons key secret))
         t))))
 
 
 ;; --------------------------------------------------------------------------
-(defun skj-secrets-value (key &optional default)
+(defun skj/secrets-value (key &optional default)
   "Return the secret value associated with the symbol KEY, or the
 value DEFAULT."
   (interactive "SKey: ")
-  (skj-secrets--validate)
-  (let ((value (alist-get key skj-secrets--alist default)))
+  (skj/secrets--validate)
+  (let ((value (alist-get key skj/secrets--alist default)))
     (when (called-interactively-p 'any)
       (message "Secret %s: %s" key value))
     value))
@@ -96,11 +94,11 @@ value DEFAULT."
 
 ;; --------------------------------------------------------------------------
 ;; --------------------------------------------------------------------------
-(unless (not skj-secrets-file)
+(unless (not skj/secrets-file)
   ;; Load the encrypted data using GPG integration.
-  (init-message "[skj-secrets] Loading external secrets (secure) file")
+  (message "[skj-secrets] Loading external secrets (secure) file")
   ;; TODO: test whether this file exists, OR create?
-  (load-file skj-secrets-file))
+  (load-file skj/secrets-file))
 
 
 (provide 'skj-secrets)
